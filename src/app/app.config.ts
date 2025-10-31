0e98511ad6f67ed966f43da22bd299b3132126f4
reloaddev <henrik.pruess@posteo.de>
2025-10-31 21:23:39 +0100

    Allow connecting web app to emulators (local development)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
+import {provideFirestore, getFirestore, connectFirestoreEmulator} from '@angular/fire/firestore';
+import {provideAuth, getAuth, connectAuthEmulator} from '@angular/fire/auth';
 import { firebaseConfig } from './firebase.config';
 import { provideServiceWorker } from '@angular/service-worker';
+import {connectFunctionsEmulator, getFunctions, provideFunctions} from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,8 +17,28 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     // AngularFire: Firebase App, Firestore and Auth
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()), provideServiceWorker('ngsw-worker.js', {
+    provideFirestore(() => {
+      const firestore = getFirestore()
+      if (location.hostname === 'localhost') {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (location.hostname === 'localhost') {
+        connectAuthEmulator(auth, 'http://localhost:9099');
+      }
+      return auth;
+    }),
+    provideFunctions(() => {
+      const fn = getFunctions();
+      if (location.hostname === 'localhost') {
+        connectFunctionsEmulator(fn, 'localhost', 5001);
+      }
+      return fn;
+    }),
+    provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
           })
